c92a854c4df27fcb5090fbe5b5a56970a896a40c
Joshua Johnston <jojohnston@kaplan.edu>
2014-12-10 16:15:30 -0500

    Fixing name so it can be installed properly

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 setup(
-    name="SmarterMeasure REST Client",
+    name="SmarterMeasure",
     version="0.1-pre",
     packages=find_packages(),
     author="Joshua Johnston",
     author_email="johnston.joshua@gmail.com",
     description="Python client for the SmarterMeasure REST API",
+    classifiers=[
+        "Development Status :: 3 - Alpha"
+        ],
     install_requires=[
         'requests'
     ],
